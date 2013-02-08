d5f78d02e9b02ecbe817aa17df91ef3774133ac1
bobeast <bobeast@adobe.com>
2013-02-08 15:38:37 -0800

    cleanup construction

--- www/PushNotification.js
+++ www/PushNotification.js
@@ -1,28 +1,65 @@
-(function(cordova) {
-    var cordovaRef = window.PhoneGap || window.Cordova || window.cordova;
 
-	function PushNotification() {}
+var PushNotification = function() {
+};
 
-	// Call this to register for push notifications. Content of [options] depends on whether we are working with APNS (iOS) or GCM (Android)
-	PushNotification.prototype.register = function(successCallback, errorCallback, options) {
-		cordovaRef.exec(successCallback, errorCallback, "PushPlugin", "register", [options]);
-	};
 
-    // Call this to unregister for push notifications
-    PushNotification.prototype.unregister = function(successCallback, errorCallback) {
-        cordovaRef.exec(successCallback, errorCallback, "PushPlugin", "unregister", []);
-    };
+// Call this to register for push notifications. Content of [options] depends on whether we are working with APNS (iOS) or GCM (Android)
+PushNotification.prototype.register = function(successCallback, errorCallback, options) {
+    if (errorCallback == null) { errorCallback = function() {}}
+
+    if (typeof errorCallback != "function")  {
+        console.log("PushNotification.register failure: failure parameter not a function");
+        return
+    }
+
+    if (typeof successCallback != "function") {
+        console.log("PushNotification.register failure: success callback parameter must be a function");
+        return
+    }
+
+	cordovaRef.exec(successCallback, errorCallback, "PushPlugin", "register", [options]);
+};
+
+// Call this to unregister for push notifications
+PushNotification.prototype.unregister = function(successCallback, errorCallback) {
+    if (errorCallback == null) { errorCallback = function() {}}
+
+    if (typeof errorCallback != "function")  {
+        console.log("PushNotification.unregister failure: failure parameter not a function");
+        return
+    }
+
+    if (typeof successCallback != "function") {
+        console.log("PushNotification.unregister failure: success callback parameter must be a function");
+        return
+    }
+
+     cordovaRef.exec(successCallback, errorCallback, "PushPlugin", "unregister", []);
+};
  
  
-    // Call this to set the application icon badge
-    PushNotification.prototype.setApplicationIconBadgeNumber = function(successCallback, badge) {
-        cordovaRef.exec(successCallback, successCallback, "PushPlugin", "setApplicationIconBadgeNumber", [{badge: badge}]);
-    };
+// Call this to set the application icon badge
+PushNotification.prototype.setApplicationIconBadgeNumber = function(successCallback, badge) {
+    if (errorCallback == null) { errorCallback = function() {}}
 
- cordova.addConstructor(function() {
-		if(!window.plugins)
-            window.plugins = {};
-		window.plugins.pushNotification = new PushNotification();
-	});
+    if (typeof errorCallback != "function")  {
+        console.log("PushNotification.setApplicationIconBadgeNumber failure: failure parameter not a function");
+        return
+    }
 
- })(window.cordova || window.Cordova || window.PhoneGap);
+    if (typeof successCallback != "function") {
+        console.log("PushNotification.setApplicationIconBadgeNumber failure: success callback parameter must be a function");
+        return
+    }
+
+    cordovaRef.exec(successCallback, successCallback, "PushPlugin", "setApplicationIconBadgeNumber", [{badge: badge}]);
+};
+
+//-------------------------------------------------------------------
+
+if(!window.plugins) {
+    window.plugins = {};
+}
+if (!window.plugins.pushNotification) {
+    window.plugins.pushNotification = new PushNotification();
+}
