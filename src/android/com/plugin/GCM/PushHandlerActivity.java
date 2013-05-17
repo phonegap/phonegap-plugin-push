30a413b8c90207396cfaee1949032baa2615ebae
Guido Smeets <g.smeets+git@gmail.com>
2013-05-17 09:11:51 +0200

    Updated to Cordova 2.7, fixed a number of bugs. Cleaned up some code.
    * Put all the plugin code in the plugin namespace: com.plugin.gcm;
    * Renamed to namespace GCM to gcm, namespaces should be lower case
    according to the java style guidelines;
    * Deleted the google gcm code and added gcm.jar instead;
    * Added CordovaGCMBroadcastReceiver.java to enable GCMIntentService to
    be namespaced in the plugin namespace instead of the client project
    (which is is the default with google's GCMIntentService);
    * Moved all java <--> javascript interop to PushPlugin;
    * Moved all notification code to GCMIntentService (maybe this fits
    better in its own class);
    * Changed the way the plugin detects cold starts in order for it not to
    crash when the back button is used to navigate out of the app;
    * Changed the caching on the filesystem to a memory cache on a static
    variable, which increases performance and simplifies code;
    * Cut up some code into separate methods ;
    * Added code comments;
    * Updated the readme file.

--- src/android/com/plugin/GCM/PushHandlerActivity.java
+++ src/android/com/plugin/GCM/PushHandlerActivity.java
@@ -1,19 +1,6 @@
-//
-//  PushHandlerActivity.java
-//
-
-package com.plugin.GCM;
-
-import java.io.FileOutputStream;
-import java.util.Iterator;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+package com.plugin.gcm;
 
 import android.app.Activity;
-import android.app.NotificationManager;
-import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
@@ -21,159 +8,59 @@ import android.util.Log;
 
 public class PushHandlerActivity extends Activity
 {
-	public static final int NOTIFICATION_ID = 237;
-	public static boolean EXITED = false;
-	
-    // this activity will be started if the user touches a notification that we own. If returning from the background,
-	// we process it immediately. If from a cold start, we cache the payload and start the main activity which will then process the cached payload.
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-		Log.v("onCreate:", "entry");
+	private static String TAG = "PushHandlerActivity"; 
 
-        super.onCreate(savedInstanceState);
+	/*
+	 * this activity will be started if the user touches a notification that we own. 
+	 * We send it's data off to the push plugin for processing.
+	 * If needed, we boot up the main activity to kickstart the application. 
+	 * @see android.app.Activity#onCreate(android.os.Bundle)
+	 */
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		Log.v(TAG, "onCreate");
 
-        Bundle extras = getIntent().getExtras();
-        if (extras != null)
-        {
-    		Bundle	originalExtras = extras.getBundle("pushBundle");
-    		if (originalExtras != null)
-        	{
-              	if (EXITED)
-                {
-            	    PackageManager pm = getPackageManager();
-            		Intent launchIntent = pm.getLaunchIntentForPackage(getApplicationContext().getPackageName());    		
-         		
-                	// remember how we got here
-            		originalExtras.putBoolean("coldstart", true);
-                		
-                	// serialize and cache the payload before starting the main activity.
-            		String json = extrasToJSON(originalExtras).toString();
-                	try
-                	{
-                		FileOutputStream fos = openFileOutput("cached_payload", Context.MODE_PRIVATE);
-                		fos.write(json.getBytes());
-                		fos.close();
-                	}
-                	catch (Exception e)
-                	{
-                		e.printStackTrace();
-                	}
-
-                	// now fire up our main activity
-                	startActivity(launchIntent);      
-                }
-              	else
-              	{
-              		// our main activity was already running (in the background), process the payload
-            		sendToApp(originalExtras);
-        		}
-
-				// Now that we've processed the notification, remove it from the tray
-				CharSequence appName = this.getPackageManager().getApplicationLabel(this.getApplicationInfo());
-        		if (null == (String)appName)
-					appName = "";
-        
-        		NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        		mNotificationManager.cancel((String) appName, NOTIFICATION_ID);
-			}
-        }
-    	
-        finish();
-    }
-    
-    // surface the notification up to the JS layer
-    public static void sendToApp(Bundle extras)
-    {
-    	JSONObject json = extrasToJSON(extras);
-    	
-    	PushPlugin.sendJavascript( json );
-    }
-    
-    // serialize the bundle for caching or JS processing
-    private static JSONObject extrasToJSON(Bundle extras)
-    {
-		try
-		{
-			JSONObject json;
-			json = new JSONObject().put("event", "message");
-        
-			JSONObject jsondata = new JSONObject();
-			Iterator<String> it = extras.keySet().iterator();
-			while (it.hasNext())
-			{
-				String key = it.next();
-				String value = extras.getString(key);	
-        	
-				// System data from Android
-				if (key.equals("from") || key.equals("collapse_key"))
-				{
-					json.put(key, value);
-				}
-				else if (key.equals("foreground"))
-				{
-					json.put(key, extras.getBoolean("foreground"));
-				}
-				else if (key.equals("coldstart"))
-				{
-					json.put(key, extras.getBoolean("coldstart"));
-				}
-				else
-				{
-					// Maintain backwards compatibility
-					if (key.equals("message") || key.equals("msgcnt") || key.equals("soundname"))
-					{
-						json.put(key, value);
-					}
-        		
-					// Try to figure out if the value is another JSON object
-					if (value.startsWith("{"))
-					{
-						try
-						{
-							JSONObject json2 = new JSONObject(value);
-							jsondata.put(key, json2);
-						}
-						catch (Exception e)
-						{
-							jsondata.put(key, value);
-						}
-						// Try to figure out if the value is another JSON array
-					}
-					else if (value.startsWith("["))
-					{
-						try
-						{
-							JSONArray json2 = new JSONArray(value);
-							jsondata.put(key, json2);
-						}
-						catch (Exception e)
-						{
-							jsondata.put(key, value);
-						}
-					}
-					else
-					{
-						jsondata.put(key, value);
-					}
-				}
-			} // while
-			json.put("payload", jsondata);
-        
-			Log.v("extrasToJSON:", json.toString());
-
-			return json;
+		boolean isPushPluginActive = PushPlugin.isActive(); 
+		if (!isPushPluginActive) {
+			forceMainActivityReload();
 		}
-		catch( JSONException e)
-		{
-			Log.e("extrasToJSON:", "JSON exception");
-		}        	
-		return null;      	
-    }
+		processPushBundle(isPushPluginActive);
 
-    @Override
-    protected void onNewIntent(Intent intent)
-    {
-        super.onNewIntent(intent);
-    }
+		GCMIntentService.cancelNotification(this);
+
+		finish();
+	}
+
+	/**
+	 * Takes the pushBundle extras from the intent, 
+	 * and sends it through to the PushPlugin for processing.
+	 */
+	private void processPushBundle(boolean isPushPluginActive)
+	{
+		Bundle extras = getIntent().getExtras();
+
+		if (extras != null)	{
+			
+			Bundle originalExtras = extras.getBundle("pushBundle");
+
+			if ( !isPushPluginActive ) { 
+				originalExtras.putBoolean("coldstart", true);
+			}
+
+			PushPlugin.sendExtras(originalExtras);
+		}
+	}
+
+	/**
+	 * Forces the main activity to re-launch if it's unloaded.
+	 */
+	private void forceMainActivityReload()
+	{
+		PackageManager pm = getPackageManager();
+		Intent launchIntent = pm.getLaunchIntentForPackage(getApplicationContext().getPackageName());    		
+		startActivity(launchIntent);
+	}
+
 }
---
+++ src/android/com/plugin/gcm/GCMIntentService.java
@@ -0,0 +1,163 @@
+package com.plugin.gcm;
+
+import java.util.List;
+
+import com.google.android.gcm.GCMBaseIntentService;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.annotation.SuppressLint;
+import android.app.ActivityManager;
+import android.app.ActivityManager.RunningTaskInfo;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+@SuppressLint("NewApi")
+public class GCMIntentService extends GCMBaseIntentService {
+
+	public static final int NOTIFICATION_ID = 237;
+	private static final String TAG = "GCMIntentService";
+	
+	public GCMIntentService() {
+		super("GCMIntentService");
+	}
+
+	@Override
+	public void onRegistered(Context context, String regId) {
+
+		Log.v(TAG, "onRegistered: "+ regId);
+
+		JSONObject json;
+
+		try
+		{
+			json = new JSONObject().put("event", "registered");
+			json.put("regid", regId);
+
+			Log.v(TAG, "onRegistered: " + json.toString());
+
+			// Send this JSON data to the JavaScript application above EVENT should be set to the msg type
+			// In this case this is the registration ID
+			PushPlugin.sendJavascript( json );
+
+		}
+		catch( JSONException e)
+		{
+			// No message to the user is sent, JSON failed
+			Log.e(TAG, "onRegistered: JSON exception");
+		}
+	}
+
+	@Override
+	public void onUnregistered(Context context, String regId) {
+		Log.d(TAG, "onUnregistered - regId: " + regId);
+	}
+
+	@Override
+	protected void onMessage(Context context, Intent intent) {
+		Log.d(TAG, "onMessage - context: " + context);
+
+		// Extract the payload from the message
+		Bundle extras = intent.getExtras();
+		if (extras != null)
+		{
+			boolean	foreground = this.isInForeground();
+
+			extras.putBoolean("foreground", foreground);
+
+			if (foreground)
+				PushPlugin.sendExtras(extras);
+			else
+				createNotification(context, extras);
+		}
+	}
+
+	public void createNotification(Context context, Bundle extras)
+	{
+		NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+		String appName = getAppName(this);
+
+		Intent notificationIntent = new Intent(this, PushHandlerActivity.class);
+		notificationIntent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+		notificationIntent.putExtra("pushBundle", extras);
+
+		PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);		
+
+		NotificationCompat.Builder mBuilder = 
+			new NotificationCompat.Builder(context)
+				.setSmallIcon(context.getApplicationInfo().icon)
+				.setWhen(System.currentTimeMillis())
+				.setContentTitle(appName)
+				.setTicker(appName)
+				.setContentIntent(contentIntent);
+
+		String message = extras.getString("message");
+		if (message != null) {
+			mBuilder.setContentText(message);
+		} else {
+			mBuilder.setContentText("<missing message content>");
+		}
+
+		String msgcnt = extras.getString("msgcnt");
+		if (msgcnt != null) {
+			mBuilder.setNumber(Integer.parseInt(msgcnt));
+		}
+
+		mNotificationManager.notify((String) appName, NOTIFICATION_ID, mBuilder.build());
+		tryPlayRingtone();
+	}
+
+	private void tryPlayRingtone() 
+	{
+		try {
+			Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+			Ringtone r = RingtoneManager.getRingtone(getApplicationContext(), notification);
+			r.play();
+		} 
+		catch (Exception e) {
+			Log.e(TAG, "failed to play notification ringtone");
+		}
+	}
+	
+	public static void cancelNotification(Context context)
+	{
+		NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+		mNotificationManager.cancel((String)getAppName(context), NOTIFICATION_ID);	
+	}
+	
+	private static String getAppName(Context context)
+	{
+		CharSequence appName = 
+				context
+					.getPackageManager()
+					.getApplicationLabel(context.getApplicationInfo());
+		
+		return (String)appName;
+	}
+	
+	public boolean isInForeground()
+	{
+		ActivityManager activityManager = (ActivityManager) getApplicationContext().getSystemService(Context.ACTIVITY_SERVICE);
+		List<RunningTaskInfo> services = activityManager
+				.getRunningTasks(Integer.MAX_VALUE);
+
+		if (services.get(0).topActivity.getPackageName().toString().equalsIgnoreCase(getApplicationContext().getPackageName().toString()))
+			return true;
+
+		return false;
+	}	
+
+	@Override
+	public void onError(Context context, String errorId) {
+		Log.e(TAG, "onError - errorId: " + errorId);
+	}
+
+}
