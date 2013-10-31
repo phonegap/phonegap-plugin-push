069c728a19bd511207167e01f68703eec67908e7
scotthooker <scott.hooker@commerceguys.com>
2013-10-31 21:07:34 +0000

    Initially the app is not inforeground
    We can rely on the the other activity functions setting foreground to be true

--- src/android/com/plugin/gcm/PushPlugin.java
+++ src/android/com/plugin/gcm/PushPlugin.java
@@ -31,7 +31,7 @@ public class PushPlugin extends CordovaPlugin {
 	private static String gECB;
 	private static String gSenderID;
 	private static Bundle gCachedExtras = null;
-    private static boolean gForeground = true;
+    private static boolean gForeground = false;
 
 	/**
 	 * Gets the application context from cordova's main activity.
@@ -122,6 +122,25 @@ public class PushPlugin extends CordovaPlugin {
 			}
 		}
 	}
+	
+    @Override
+    public void onStart(boolean multitasking) {
+        super.onStart(multitasking);
+        gForeground = true;
+    }
+
+    @Override
+    public void onRestart(boolean multitasking) {
+        super.onRestart(multitasking);
+        gForeground = true;
+    }
+
+    @Override
+    public void onStop(boolean multitasking) {
+        super.onStop(multitasking);
+        gForeground = false;
+    }
+
 
     @Override
     public void onPause(boolean multitasking) {
