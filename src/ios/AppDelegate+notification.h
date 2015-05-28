220de9efb6e68de30ae9a424ade0563163ca8684
Grant Benvenuti <g.benvenuti@pni.com.au>
2015-05-28 15:55:04 +1000

    Added iOS8 app delage methods so that didRegisterForRemoteNotificationsWithDeviceToken is called

--- src/ios/AppDelegate+notification.h
+++ src/ios/AppDelegate+notification.h
@@ -13,8 +13,10 @@
 - (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error;
 - (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo;
 - (void)applicationDidBecomeActive:(UIApplication *)application;
+- (void)application:(UIApplication *)application didRegisterUserNotificationSettings:(UIUserNotificationSettings *)notificationSettings;
+- (void)application:(UIApplication *)application handleActionWithIdentifier:(NSString *)identifier forRemoteNotification:(NSDictionary *)userInfo completionHandler:(void(^)())completionHandler;
 - (id) getCommandInstance:(NSString*)className;
 
-@property (nonatomic, retain) NSDictionary	*launchNotification;
+@property (nonatomic, retain) NSDictionary  *launchNotification;
 
 @end
