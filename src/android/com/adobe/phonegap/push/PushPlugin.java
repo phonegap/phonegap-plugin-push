58467578c7fbbd3a7d9025b287fe8cb7ccd076cf
Simon MacDonald <simon.macdonald@gmail.com>
2015-11-02 18:02:39 -0500

    Issue #291: Reregister on Android

--- src/android/com/adobe/phonegap/push/PushPlugin.java
+++ src/android/com/adobe/phonegap/push/PushPlugin.java
@@ -127,6 +127,18 @@ public class PushPlugin extends CordovaPlugin implements PushConstants {
                     try {
                         InstanceID.getInstance(getApplicationContext()).deleteInstanceID();
                         Log.v(LOG_TAG, "UNREGISTER");
+
+                        // Remove shared prefs
+                        SharedPreferences sharedPref = getApplicationContext().getSharedPreferences(COM_ADOBE_PHONEGAP_PUSH, Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedPref.edit();
+                        editor.remove(SOUND);
+                        editor.remove(VIBRATE);
+                        editor.remove(CLEAR_NOTIFICATIONS);
+                        editor.remove(FORCE_SHOW);
+                        editor.remove(SENDER_ID);
+                        editor.remove(REGISTRATION_ID);
+                        editor.commit();
+
                         callbackContext.success();
                     } catch (IOException e) {
                         Log.e(LOG_TAG, "execute: Got JSON Exception " + e.getMessage());
