386958969f1eab42d732454f6ab0ae9fe007fd28
Adri Van Houdt <adri.van.houdt@gmail.com>
2015-11-27 17:28:05 +0100

    did I say cleanup before?

--- src/ios/PushPlugin.m
+++ src/ios/PushPlugin.m
@@ -319,11 +319,11 @@
         UIApplication *app = [UIApplication sharedApplication];
         float finishTimer = (app.backgroundTimeRemaining > 20.0) ? 20.0 : app.backgroundTimeRemaining;
     
-        [NSTimer scheduledTimerWithTimeInterval:[finishTimer
-                                     target:self
-                                    selector:@selector(stopBackgroundTask:)
-                                    userInfo:nil
-                                     repeats:NO];
+        [NSTimer scheduledTimerWithTimeInterval:finishTimer
+                                    target:self
+                                   selector:@selector(stopBackgroundTask:)
+                                   userInfo:nil
+                                    repeats:NO];
 
         CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
         [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
