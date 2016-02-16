f9f33360fc9d706e1c66ab77c77460f491938fd9
Barry vd. Heuvel <barryvdh@gmail.com>
2016-02-16 11:49:53 +0100

    Add setApplicationIconBadgeNumber action for Android, using ShortcutBadger

--- src/android/com/adobe/phonegap/push/PushConstants.java
+++ src/android/com/adobe/phonegap/push/PushConstants.java
@@ -54,4 +54,5 @@ public interface PushConstants {
     public static final String GCM = "GCM";
     public static final String CONTENT_AVAILABLE = "content-available";
     public static final String TOPICS = "topics";
+    public static final String SET_APPLICATION_ICON_BADGE_NUMBER = "setApplicationIconBadgeNumber";
 }
