4309ee35fc965ce9135b0c60f33598546bbaf8b1
Michell <michellarcari@hotmail.com>
2016-04-06 11:37:20 -0300

    #776 - Added Android Wear features
    Extended the notification action buttons to Android Wear

--- src/android/com/adobe/phonegap/push/GCMIntentService.java
+++ src/android/com/adobe/phonegap/push/GCMIntentService.java
@@ -16,6 +16,8 @@ import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.support.v4.app.NotificationCompat.WearableExtender;
 import android.text.Html;
 import android.util.Log;
 
@@ -333,6 +335,7 @@ public class GCMIntentService extends GcmListenerService implements PushConstant
         if (actions != null) {
             try {
                 JSONArray actionsArray = new JSONArray(actions);
+                ArrayList<NotificationCompat.Action> wActions = new ArrayList<NotificationCompat.Action>();
                 for (int i=0; i < actionsArray.length(); i++) {
                     Log.d(LOG_TAG, "adding action");
                     JSONObject action = actionsArray.getJSONObject(i);
@@ -355,10 +358,18 @@ public class GCMIntentService extends GcmListenerService implements PushConstant
                         intent.putExtra(NOT_ID, notId);
                         pIntent = PendingIntent.getBroadcast(this, i, intent, PendingIntent.FLAG_UPDATE_CURRENT);
                     }
-
+                    NotificationCompat.Action wAction =
+                    new NotificationCompat.Action.Builder(resources.getIdentifier(action.optString(ICON, ""), DRAWABLE, packageName),
+                            action.getString(TITLE), pIntent)
+                            .build();
+                    wActions.add(wAction);
                     mBuilder.addAction(resources.getIdentifier(action.optString(ICON, ""), DRAWABLE, packageName),
                             action.getString(TITLE), pIntent);
+                    wAction = null;
+                    pIntent = null;
                 }
+                mBuilder.extend(new WearableExtender().addActions(wActions));
+                wActions.clear();
             } catch(JSONException e) {
                 // nope
             }
