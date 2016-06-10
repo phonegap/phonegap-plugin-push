f67e30d41f5ea94a60a53b00ed51b995960f89c9
jcesarmobile <jcesarmobile@gmail.com>
2016-06-10 19:41:23 +0200

    Added partial Android FCM support (#975)
    * Modified plugin.xml to include FCM changes
    
    * Plugin.xml changes for FCM
    
    * Java changes for FCM
    
    * Increased plugin version to 2.0.0
    
    * Increased plugin version in package.json
    
    * Added topic subscription/unsubscription
    
    * Removed some empty lines

--- src/android/com/adobe/phonegap/push/GCMIntentService.java
+++ src/android/com/adobe/phonegap/push/GCMIntentService.java
@@ -28,7 +28,8 @@ import android.text.Html;
 import android.text.Spanned;
 import android.util.Log;
 
-import com.google.android.gms.gcm.GcmListenerService;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -41,10 +42,11 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
 import java.util.Random;
 
 @SuppressLint("NewApi")
-public class GCMIntentService extends GcmListenerService implements PushConstants {
+public class GCMIntentService extends FirebaseMessagingService implements PushConstants {
 
     private static final String LOG_TAG = "Push_GCMIntentService";
     private static HashMap<Integer, ArrayList<String>> messageMap = new HashMap<Integer, ArrayList<String>>();
@@ -64,8 +66,19 @@ public class GCMIntentService extends GcmListenerService implements PushConstant
     }
 
     @Override
-    public void onMessageReceived(String from, Bundle extras) {
-        Log.d(LOG_TAG, "onMessage - from: " + from);
+    public void onMessageReceived(RemoteMessage message){
+
+        Log.d(LOG_TAG, "onMessage - from: " + message.getFrom());
+
+        Bundle extras = new Bundle();
+
+        if (message.getNotification()!=null) {
+            extras.putString(TITLE,message.getNotification().getTitle());
+            extras.putString(MESSAGE,message.getNotification().getBody());
+        }
+        for (Map.Entry<String, String> entry : message.getData().entrySet()) {
+            extras.putString(entry.getKey(), entry.getValue());
+        }
 
         if (extras != null && isAvailableSender(from)) {
             Context applicationContext = getApplicationContext();
@@ -709,7 +722,7 @@ public class GCMIntentService extends GcmListenerService implements PushConstant
         if (bitmap == null) {
             return null;
         }
-        
+
         final Bitmap output = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), Bitmap.Config.ARGB_8888);
         final Canvas canvas = new Canvas(output);
         final int color = Color.RED;
