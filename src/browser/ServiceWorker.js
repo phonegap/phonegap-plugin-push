63a29cdbbc0cd0374552d51e0e2d2217f5361f79
Simon MacDonald <simon.macdonald@gmail.com>
2016-07-25 14:26:07 -0400

    Populate additionalData on browser platform

--- src/browser/ServiceWorker.js
+++ src/browser/ServiceWorker.js
@@ -7,31 +7,41 @@ self.addEventListener('install', function(event) {
 self.addEventListener('push', function(event) {
     // parse incoming message
     var obj = {};
+    var pushData = {
+        image: 'https://avatars1.githubusercontent.com/u/60365?v=3&s=200',
+        additionalData: {}
+    };
     if (event.data) {
         obj = event.data.json();
     }
 
-    // Need to figure out a way to make these configurable
-    var title = obj.title || 'Default title';
-    var body = obj.body || 'This is the default body';
-    var icon = obj.image || 'https://avatars1.githubusercontent.com/u/60365?v=3&s=200';
-    var tag = 'simple-push-demo-notification-tag';
+    console.log(obj);
+
+    // convert to push plugin API
+    for (var key in obj) {
+        if (key === 'title') {
+            pushData.title = obj[key];
+        } else if (key === 'message' || key === 'body') {
+            pushData.message = obj[key];
+        } else if (key === 'count' || key === 'msgcnt' || key === 'badge') {
+            pushData.count = obj[key];
+        } else if (key === 'sound' || key === 'soundname') {
+            pushData.sound = obj[key];
+        } else if (key === 'image') {
+            pushData.image = obj[key];
+        } else {
+            pushData.additionalData[key] = obj[key];
+        }
+    }
 
     event.waitUntil(
-        self.registration.showNotification(title, {
-            body: body,
-            icon: icon,
-            tag: tag
+        self.registration.showNotification(pushData.title, {
+            body: pushData.message,
+            icon: pushData.image,
+            tag: 'simple-push-demo-notification-tag'
         })
     );
 
-    var pushData = {
-        title: title,
-        message: body,
-        count: 1,
-        sound: 'default',
-        additionalData: {}
-    };
     messageChannel.ports[0].postMessage(pushData);
 
 });
