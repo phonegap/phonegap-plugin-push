bef73be450b690ba057140d9a591f85765788ff6
Simon MacDonald <simon.macdonald@gmail.com>
2016-11-24 18:04:25 -0500

    Issue #689: Remove sender id from PushNotification init Android options

--- src/android/com/adobe/phonegap/push/PushConstants.java
+++ src/android/com/adobe/phonegap/push/PushConstants.java
@@ -79,4 +79,5 @@ public interface PushConstants {
     public static final String IMAGE_TYPE_SQUARE = "square";
     public static final String IMAGE_TYPE_CIRCLE = "circle";
     public static final String SUBJECT = "subject";
+    public static final String GOOGLE_APP_ID = "google_app_id";
 }
