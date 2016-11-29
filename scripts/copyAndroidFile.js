b41580f7476a34014ceb03976f25989f98cbde6a
Simon MacDonald <simon.macdonald@gmail.com>
2016-11-29 10:55:59 -0500

    :bug: Issue #1188: Strings.xml google_app_id conflict with google-services.json

---
+++ scripts/copyAndroidFile.js
@@ -0,0 +1,34 @@
+module.exports = function(ctx) {
+    var fs = ctx.requireCordovaModule('fs'),
+        path = ctx.requireCordovaModule('path'),
+        os = require("os"),
+        readline = require("readline"),
+        deferral = ctx.requireCordovaModule('q').defer();
+
+    var settingsFile = path.join(ctx.opts.projectRoot, 'google-services.json');
+
+    fs.stat(settingsFile, function(err,stats) {
+        if (err) {
+            deferral.reject("To use this plugin on android you'll need to add a google-services.json file with the FCM project_info and place that into your www folder");
+        } else {
+
+            fs.createReadStream(settingsFile).pipe(fs.createWriteStream('platforms/android/google-services.json'));
+
+            var lineReader = readline.createInterface({
+                terminal: false,
+                input : fs.createReadStream('platforms/android/build.gradle')
+            });
+            lineReader.on("line", function(line) {
+                fs.appendFileSync('./build.gradle', line.toString() + os.EOL);
+                if (/.*\ dependencies \{.*/.test(line)) {
+                    fs.appendFileSync('./build.gradle', '\t\tclasspath "com.google.gms:google-services:3.0.0"' + os.EOL);
+                }
+            }).on("close", function () {
+                fs.rename('./build.gradle', 'platforms/android/build.gradle', deferral.resolve);
+            });
+
+        }
+    });
+
+    return deferral.promise;
+};
--- src/android/com/adobe/phonegap/push/PushConstants.java
+++ src/android/com/adobe/phonegap/push/PushConstants.java
@@ -79,4 +79,5 @@ public interface PushConstants {
     public static final String IMAGE_TYPE_SQUARE = "square";
     public static final String IMAGE_TYPE_CIRCLE = "circle";
     public static final String GOOGLE_APP_ID = "google_app_id";
+    public static final String GCM_DEFAULT_SENDER_ID = "gcm_defaultSenderId";
 }
