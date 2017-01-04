704b3635764700d5fc06f2e9c8c6a657ea4b7f29
Simon MacDonald <simon.macdonald@gmail.com>
2017-01-04 15:48:49 -0500

    :apple::bug: Issue #1497: App crashes after refreshing when using FCM (v2)

--- src/ios/PushPlugin.m
+++ src/ios/PushPlugin.m
@@ -330,7 +330,8 @@
             NSLog(@"Using FCM Notification");
             [self setUsesFCM: YES];
             dispatch_async(dispatch_get_main_queue(), ^{
-                [FIRApp configure];
+                if([FIRApp defaultApp] == nil)
+                    [FIRApp configure];
                 [self initRegistration];
             });
         } else {
