0b5fedca276252c15c1716d69a958230f2e3f575
Simon MacDonald <simon.macdonald@gmail.com>
2017-05-06 00:41:42 +0200

    Add resource-file way of copying google services files

--- scripts/copyAndroidFile.js
+++ scripts/copyAndroidFile.js
@@ -5,29 +5,17 @@ module.exports = function(ctx) {
         readline = require("readline"),
         deferral = ctx.requireCordovaModule('q').defer();
 
-    var settingsFile = path.join(ctx.opts.projectRoot, 'google-services.json');
-
-    fs.stat(settingsFile, function(err,stats) {
-        if (err) {
-            deferral.reject("To use this plugin on android you'll need to add a google-services.json file with the FCM project_info and place that into your project's root folder");
-        } else {
-
-            fs.createReadStream(settingsFile).pipe(fs.createWriteStream('platforms/android/google-services.json'));
-
-            var lineReader = readline.createInterface({
-                terminal: false,
-                input : fs.createReadStream('platforms/android/build.gradle')
-            });
-            lineReader.on("line", function(line) {
-                fs.appendFileSync('./build.gradle', line.toString() + os.EOL);
-                if (/.*\ dependencies \{.*/.test(line)) {
-                    fs.appendFileSync('./build.gradle', '\t\tclasspath "com.google.gms:google-services:3.0.0"' + os.EOL);
-                }
-            }).on("close", function () {
-                fs.rename('./build.gradle', 'platforms/android/build.gradle', deferral.resolve);
-            });
-
+    var lineReader = readline.createInterface({
+        terminal: false,
+        input : fs.createReadStream('platforms/android/build.gradle')
+    });
+    lineReader.on("line", function(line) {
+        fs.appendFileSync('./build.gradle', line.toString() + os.EOL);
+        if (/.*\ dependencies \{.*/.test(line)) {
+            fs.appendFileSync('./build.gradle', '\t\tclasspath "com.google.gms:google-services:3.0.0"' + os.EOL);
         }
+    }).on("close", function () {
+        fs.rename('./build.gradle', 'platforms/android/build.gradle', deferral.resolve);
     });
 
     return deferral.promise;
