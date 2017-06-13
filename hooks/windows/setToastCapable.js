990586520f01bbac0075691e5398b98a8c6c7ddc
brett rudd <brettrudd@gmail.com>
2017-06-13 14:10:13 -0700

    :bug: Issue #1785: plugin fails to install on windows using plugman

--- hooks/windows/setToastCapable.js
+++ hooks/windows/setToastCapable.js
@@ -1,7 +1,13 @@
 module.exports = function(context) {
-    console.log('Updating appxmanifests with ToastCapable=true...');
+    console.log('Updating appxmanifests with ToastCapable=true ...');
     var path = require('path');
+    var fs = require('fs');
+
     var platformProjPath = path.join(context.opts.projectRoot, 'platforms/windows');
+    if (!fs.existsSync(platformProjPath)) {
+        platformProjPath = context.opts.projectRoot;
+    }
+    
     var AppxManifest = require(path.join(platformProjPath, 'cordova/lib/AppxManifest'));
 
     ['package.phone.appxmanifest', 'package.windows.appxmanifest'].forEach(function(manifestPath) {
