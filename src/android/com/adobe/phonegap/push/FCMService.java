118b1a454eebc9e5b053f40be3f1a3af253d783e
Nitro Ghost <wildeman.benjamin@gmail.com>
2017-06-30 21:28:40 +0100

    V2.x Android: handle dismissed notifications (#1816)
    * handle dismissed notifications
    
    * added missing const
    
    * :penguin: :bug: obligatory commit message

--- src/android/com/adobe/phonegap/push/FCMService.java
+++ src/android/com/adobe/phonegap/push/FCMService.java
@@ -362,11 +362,14 @@ public class FCMService extends FirebaseMessagingService implements PushConstant
         int requestCode = new Random().nextInt();
         PendingIntent contentIntent = PendingIntent.getActivity(this, requestCode, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
 
-        Intent dismissedNotificationIntent = new Intent(notificationIntent);
+        Intent dismissedNotificationIntent = new Intent(this, PushDismissedHandler.class);
+        dismissedNotificationIntent.putExtra(PUSH_BUNDLE, extras);
+        dismissedNotificationIntent.putExtra(NOT_ID, notId);
         dismissedNotificationIntent.putExtra(DISMISSED, true);
+        dismissedNotificationIntent.setAction(PUSH_DISMISSED);
 
         requestCode = new Random().nextInt();
-        PendingIntent deleteIntent = PendingIntent.getActivity(this, requestCode, dismissedNotificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        PendingIntent deleteIntent = PendingIntent.getBroadcast(this, requestCode, dismissedNotificationIntent, PendingIntent.FLAG_CANCEL_CURRENT);
 
         NotificationCompat.Builder mBuilder =
                 new NotificationCompat.Builder(context)
@@ -374,7 +377,7 @@ public class FCMService extends FirebaseMessagingService implements PushConstant
                         .setContentTitle(fromHtml(extras.getString(TITLE)))
                         .setTicker(fromHtml(extras.getString(TITLE)))
                         .setContentIntent(contentIntent)
-                        //.setDeleteIntent(deleteIntent)
+                        .setDeleteIntent(deleteIntent)
                         .setAutoCancel(true);
 
         SharedPreferences prefs = context.getSharedPreferences(PushPlugin.COM_ADOBE_PHONEGAP_PUSH, Context.MODE_PRIVATE);
---
+++ src/android/com/adobe/phonegap/push/PushDismissedHandler.java
@@ -0,0 +1,25 @@
+package com.adobe.phonegap.push;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+public class PushDismissedHandler extends BroadcastReceiver implements PushConstants {
+    private static String LOG_TAG = "Push_DismissedHandler";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Bundle extras = intent.getExtras();
+        FCMService fcm = new FCMService();
+        String action = intent.getAction();
+        int notID = intent.getIntExtra(NOT_ID, 0);
+
+        if (action.equals(PUSH_DISMISSED)) {
+            Log.d(LOG_TAG, "PushDismissedHandler = " + extras);
+            Log.d(LOG_TAG, "not id = " + notID);
+
+            fcm.setNotification(notID, "");
+        }
+    }
+}
