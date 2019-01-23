be66f9249f7c233ec02a51585b433f545400404d
Graham Mackenzie <gmackenzie@commontime.com>
2019-01-23 13:47:09 +0000

    CM-129: Missing constants

--- src/android/com/adobe/phonegap/push/PushConstants.java
+++ src/android/com/adobe/phonegap/push/PushConstants.java
@@ -102,4 +102,7 @@ public interface PushConstants {
   public static final String ONGOING = "ongoing";
   public static final String LIST_CHANNELS = "listChannels";
   public static final String CLEAR_NOTIFICATION = "clearNotification";
+
+  public static final String BRING_TO_FRONT = "bringToFront";
+  public static final String PUSH_START = "pushStart"
 }
