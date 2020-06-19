7ddacb090e9cb87abaf6fd6e226fb6180c6b4489
Andrei Nartea <duca14036@gmail.com>
2020-06-19 22:47:07 +0300

    Release 2.3.1 register for push notification like in FCM doc

--- src/ios/PushPlugin.m
+++ src/ios/PushPlugin.m
@@ -29,7 +29,6 @@
 #import "PushPlugin.h"
 #import "AppDelegate+notification.h"
 @import Firebase;
-@import FirebaseAnalytics;
 
 @implementation PushPlugin : CDVPlugin
 
@@ -688,6 +687,27 @@
 
 - (void)registerForRemoteNotifications
 {
+    if ([UNUserNotificationCenter class] != nil) {
+      // iOS 10 or later
+      // For iOS 10 display notification (sent via APNS)
+        
+      [UNUserNotificationCenter currentNotificationCenter].delegate = self;
+      UNAuthorizationOptions authOptions = UNAuthorizationOptionAlert |
+          UNAuthorizationOptionSound | UNAuthorizationOptionBadge;
+      [[UNUserNotificationCenter currentNotificationCenter]
+          requestAuthorizationWithOptions:authOptions
+          completionHandler:^(BOOL granted, NSError * _Nullable error) {
+            // ...
+          }];
+    } else {
+      // iOS 10 notifications aren't available; fall back to iOS 8-9 notifications.
+      UIUserNotificationType allNotificationTypes =
+      (UIUserNotificationTypeSound | UIUserNotificationTypeAlert | UIUserNotificationTypeBadge);
+      UIUserNotificationSettings *settings =
+      [UIUserNotificationSettings settingsForTypes:allNotificationTypes categories:nil];
+      [[UIApplication sharedApplication] registerUserNotificationSettings:settings];
+    }
+    
     [[UIApplication sharedApplication] registerForRemoteNotifications];
 }
 
